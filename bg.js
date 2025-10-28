9680c2fb3e3e3885961d99bd39176c0a285bf065
Ryan <38592470+asperand@users.noreply.github.com>
2025-10-28 13:15:01 -0400

    Implement basic WebGL canvas setup

---
+++ bg.js
@@ -0,0 +1,12 @@
+main();
+
+function main() {
+  const canvas = document.querySelector("#bg-canvas");
+  const gl = canvas.getContext("webgl");
+  if (gl == null){
+    alert("Your browser does not support WebGL :(");
+    return;
+  }
+  gl.clearColor(0.0,0.0,0.0,1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
