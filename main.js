92218fa9f6558ef029308b45ccf6c8481605b16f
asperand <rjm99@proton.me>
2025-10-29 11:07:26 -0400

    next step, learn textures

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -13,10 +13,9 @@ const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 1;
 
 function animate(){
-	cube.rotation.x+=0.01;
-	cube.rotation.y+=0.01;
+	
   	renderer.render(scene,camera);
 }
